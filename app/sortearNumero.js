41ad0c0a77cb32eea5af170c13bffdec87d0d9ac
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-14 11:21:40 -0300

    Criando ouvinte click, para acionar a função de gerar o numero aleatorio que utiliza o math.random

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,7 +1,11 @@
 const numeroSecreto = gerarNumeroAleatorio();
+const box = document.querySelector('.box');
+const botao = document.querySelector('.teste');
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 100);
 }
 
-console.log(numeroSecreto)
+botao.addEventListener('click', () => {
+    console.log(gerarNumeroAleatorio())
+});
