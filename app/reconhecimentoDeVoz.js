43d6f051960708549530ebcadf21d301850cff65
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-14 14:20:32 -0300

    Capturando o reconhecimento de voz e exibindo na tela

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -2,6 +2,8 @@ const SpeechRecognition =
 //window é um objeto global que representa a janela do navegador.
   window.SpeechRecognition || window.webkitSpeechRecognition; //Determina QUAL implementação da API será usada pelo no navegador usado
 
+let boxVoceDisse = document.querySelector('.box');
+
 //Instância do reconhecimento de voz
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
@@ -10,10 +12,10 @@ recognition.addEventListener('result', onSpeak);
 
 function onSpeak(e) {
     console.log(e.results[0][0].transcript)
-    const voceDisse = e.results[0][0].transcript;
-    if(voceDisse == 20) {
-        console.log('palmeiras n tem mundial')
+    const capturaDeVoz = e.results[0][0].transcript;
+    if(capturaDeVoz) {
+        boxVoceDisse.textContent = capturaDeVoz;
     } else {
-        console.log('fortnite lego é melhor que minecraft')
+        boxVoceDisse.textContent = 'erro';
     }
 }
