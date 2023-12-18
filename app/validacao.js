95e0d480c1a057edb98a67ec1c5969d2d39bfd79
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-18 16:06:27 -0300

    Criando dicas de acerto para o numero secreto

--- app/validacao.js
+++ app/validacao.js
@@ -1,23 +1,31 @@
 function verificaSeOChutePossuiUmValorValido(chuteEmFormatoDeString) {
     // Erros
     // para converter o chute em string usamos o sinal =+
-    const numero =+ chuteEmFormatoDeString;
+    const numero = + chuteEmFormatoDeString;
     if (chuteNaoEUmNumero(numero)) {
         elementoChute.innerHTML += '<div>Não é um número</div>'
+        // Early return
+        return;
     }
-    
-    if(numeroForaDoRange(numero)) {
+
+    if (numeroForaDoRange(numero)) {
         elementoChute.innerHTML += `<div>O valor precisa estar entre ${menorvalor} e ${maiorValor}</div>`;
+        // Early return
+        return;
     }
 
     // Acerto
-    if(numero === numeroSecreto) {
-        document.body.innerHTML = 
-        `
+    if (numero === numeroSecreto) {
+        document.body.innerHTML =
+            `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
-        
         `
+    // Dicas
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class=”fa-solid fa-down-long”></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class=”fa-solid fa-up-long”></i></div>`
     }
 }
 
